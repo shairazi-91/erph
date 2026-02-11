2b2c09cb11f650cfc7c1eff3837ab2d0ec88deab
shairazi-91 <muhdshairazi@gmail.com>
2026-02-11 23:07:09 +0800

    Create data_t3.js

---
+++ data_t3.js
@@ -0,0 +1,176 @@
+MASTER_DATA["Tingkatan 3 (DLP)"] = {
+    "1.0 STIMULI AND RESPONSES": {
+        "1.1 Human nervous system": [
+            "1.1.1 Describe the structures and functions of human nervous system through drawings.",
+            "1.1.2 Make a sequence to show the pathway of impulses in voluntary and involuntary actions.",
+            "1.1.3 Justify the importance of human nervous system in daily life."
+        ],
+        "1.2 Stimuli and responses in human": [
+            "1.2.1 Draw structures of sensory organs and explain their functions and sensitivities towards stimuli.",
+            "1.2.2 Explain the mechanism of hearing and sight through drawings.",
+            "1.2.3 Relate human sensory organs to sensitivity towards various combination of stimuli.",
+            "1.2.4 Explain through examples how the limitation of senses, defect in sensory organs and ageing affect human hearing and sight.",
+            "1.2.5 Justify how innovations and technologies can improve the ability of sensory organs."
+        ],
+        "1.3 Stimuli in plants": [
+            "1.3.1 Describe the parts of a plant that respond to stimuli.",
+            "1.3.2 Justify the importance of responses to stimuli in plants.",
+            "1.3.3 Carry out experiments to study responses in plants in various situations."
+        ],
+        "1.4 Importance of responses": [
+            "1.4.1 Explain with examples the type of sight and hearing in animals.",
+            "1.4.2 Communicate how sensory organs help in the survival of animals on Earth."
+        ]
+    },
+    "2.0 RESPIRATION": {
+        "2.1 Human respiratory system": [
+            "2.1.1 Draw and label the internal structures of the human respiratory system.",
+            "2.1.2 Carry out experiments to investigate the differences in the content of gases in inhaled and exhaled air."
+        ],
+        "2.2 Movement and exchange of gases in the human body": [
+            "2.2.1 Describe the movement and exchange of oxygen and carbon dioxide in the human body.",
+            "2.2.2 Justify the importance of adaptations of the alveolar structure for efficient gas exchange."
+        ],
+        "2.3 Health of human respiratory system": [
+            "2.3.1 Communicate with examples of substances that are harmful to the respiratory system.",
+            "2.3.2 Carry out an experiment to show the effects of smoking on the lungs."
+        ],
+        "2.4 Adaptations in respiratory systems": [
+            "2.4.1 Justify how the respiratory system adapts in different situations."
+        ],
+        "2.5 Gaseous exchange in plants": [
+            "2.5.1 Explain the mechanism of gaseous exchange in plants.",
+            "2.5.2 Communicate the importance of an unpolluted environment for the growth and survival of plants."
+        ]
+    },
+    "3.0 TRANSPORTATION": {
+        "3.1 Transport system in organisms": [
+            "3.1.1 Describe the function of transport systems in complex and simple organisms.",
+            "3.1.2 Compare and contrast the functions of transport systems in complex and simple organisms.",
+            "3.1.3 Justify the importance of the function of transport system in organisms."
+        ],
+        "3.2 Blood circulatory system": [
+            "3.2.1 Generalise the meaning of blood circulatory system in animals.",
+            "3.2.2 Explain the structure and functions of the human heart.",
+            "3.2.3 Carry out experiments to study factors that affect pulse rate.",
+            "3.2.4 Justify the importance of maintaining a healthy heart."
+        ],
+        "3.3 Human blood": [
+            "3.3.1 Separate the components of human blood through centrifugation.",
+            "3.3.2 Identify the blood groups and the compatibility of blood donors and recipients.",
+            "3.3.3 Communicate the importance of blood donation and the storage of blood."
+        ],
+        "3.4 Transport system in plants": [
+            "3.4.1 Describe transpiration in plants.",
+            "3.4.2 Carry out experiments to investigate the factors affecting the rate of transpiration.",
+            "3.4.3 Differentiate between the components of vascular bundles in plants."
+        ],
+        "3.5 Blood circulatory system in animals and transport system in plants": [
+            "3.5.1 Compare and contrast the blood circulatory system in animals with the transport system in plants."
+        ]
+    },
+    "4.0 REACTIVITY OF METALS": {
+        "4.1 Variety of minerals": [
+            "4.1.1 Explain with examples the natural minerals in the Earth's crust.",
+            "4.1.2 Identify the elements found in natural compounds.",
+            "4.1.3 Explain with examples the characteristics of natural minerals and their uses."
+        ],
+        "4.2 Reactivity series": [
+            "4.2.1 Construct a reactivity series of metals based on their reactions with oxygen.",
+            "4.2.2 Determine the position of carbon and hydrogen in the reactivity series of metals."
+        ],
+        "4.3 Extraction of metals from their ores": [
+            "4.3.1 Communicate about the extraction of metals from their ores.",
+            "4.3.2 Generate ideas to solve problems involving the mining of minerals in Malaysia."
+        ]
+    },
+    "5.0 THERMOCHEMISTRY": {
+        "5.1 Endothermic and exothermic reactions": [
+            "5.1.1 Define endothermic and exothermic reactions.",
+            "5.1.2 Relate heat absorbed or released in chemical reactions to endothermic and exothermic reactions.",
+            "5.1.3 Carry out experiments to compare and contrast endothermic and exothermic reactions.",
+            "5.1.4 Explain with examples endothermic and exothermic reactions.",
+            "5.1.5 Design materials or products using the concept of endothermic and exothermic reactions."
+        ]
+    },
+    "6.0 ELECTRICITY AND MAGNETISM": {
+        "6.1 Generation of electricity": [
+            "6.1.1 Describe various sources of energy used to generate electricity.",
+            "6.1.2 Explain the process of generating electricity in various power stations.",
+            "6.1.3 Differentiate between direct current (d.c.) and alternating current (a.c.).",
+            "6.1.4 Solve problems related to the generation of electricity."
+        ],
+        "6.2 Transformer": [
+            "6.2.1 Carry out an experiment to construct a step-up and a step-down transformer.",
+            "6.2.2 Communicate about transformers and their functions in the transmission and distribution of electricity.",
+            "6.2.3 Solve numerical problems using the transformer formula."
+        ],
+        "6.3 Transmission and distribution of electricity": [
+            "6.3.1 Explain the functions of components in the transmission and distribution of electricity.",
+            "6.3.2 Explain with examples the electrical wiring system at home.",
+            "6.3.3 Distinguish between safety components in the electrical wiring system at home.",
+            "6.3.4 Communicate about safety in the usage of electrical appliances."
+        ],
+        "6.4 Calculate the cost of electricity consumption": [
+            "6.4.1 Define energy efficiency.",
+            "6.4.2 List examples of technology that applies the concept of energy efficiency.",
+            "6.4.3 Determine the amount of electricity used in electrical appliances.",
+            "6.4.4 Calculate the cost of electricity consumption.",
+            "6.4.5 Carry out an audit of electrical energy consumption at home.",
+            "6.4.6 Communicate about ways of saving electrical energy usage at home."
+        ]
+    },
+    "7.0 ENERGY AND POWER": {
+        "7.1 Work, Energy and Power": [
+            "7.1.1 Define work and calculate work done.",
+            "7.1.2 Relate power with work and calculate power."
+        ],
+        "7.2 Potential Energy and Kinetic Energy": [
+            "7.2.1 Explain with examples gravitational potential energy and calculate it.",
+            "7.2.2 Calculate elastic potential energy.",
+            "7.2.3 Explain with examples kinetic energy and calculate it."
+        ],
+        "7.3 Principle of Conservation of Energy": [
+            "7.3.1 Explain the Principle of Conservation of Energy.",
+            "7.3.2 Solve qualitative and quantitative problems involving the transformation of energy."
+        ]
+    },
+    "8.0 RADIOACTIVITY": {
+        "8.1 History of the discovery of radioactivity": [
+            "8.1.1 Describe the history of the discovery of radioactivity.",
+            "8.1.2 Explain with examples radioactive materials, radioactivity and the concept of half-life."
+        ],
+        "8.2 Atom and nucleus": [
+            "8.2.1 Draw the atomic structure of an element in a stable and unstable state.",
+            "8.2.2 Explain the formation of positive and negative ions."
+        ],
+        "8.3 Ionising radiation and non-ionising radiation": [
+            "8.3.1 Describe ionising radiation and non-ionising radiation.",
+            "8.3.2 Differentiate between the three types of ionising radiation.",
+            "8.3.3 Explain with examples the sources of ionising radiation in the environment.",
+            "8.3.4 Discuss the risks of exposure to natural and man-made ionising radiation."
+        ],
+        "8.4 Uses of radioactive radiation": [
+            "8.4.1 Communicate about the use of radioactive radiation in various fields.",
+            "8.4.2 Justify the importance of proper handling of radioactive substances and radioactive waste."
+        ]
+    },
+    "9.0 SPACE WEATHER": {
+        "9.1 Activities of the Sun": [
+            "9.1.1 Explain the structure of the Sun and the phenomena that occur on the Sun's surface.",
+            "9.1.2 Justify the importance of the magnetosphere to the Earth."
+        ],
+        "9.2 Space weather": [
+            "9.2.1 Communicate about space weather and its effects on the Earth."
+        ]
+    },
+    "10.0 SPACE EXPLORATION": {
+        "10.1 Development in astronomy": [
+            "10.1.1 Explain the historical development of the models of the solar system."
+        ],
+        "10.2 Development of technology and its application in space exploration": [
+            "10.2.1 Communicate about the importance of the development of technology and its application in space exploration.",
+            "10.2.2 Justify the need to continue space exploration."
+        ]
+    }
+};
