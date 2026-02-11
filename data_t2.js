f4ab379c4ce21331fc8892e12f8e3aa8a574b346
shairazi-91 <muhdshairazi@gmail.com>
2026-02-11 23:06:14 +0800

    Update and rename data.js to data_t2.js

---
+++ data_t2.js
@@ -0,0 +1,60 @@
+MASTER_DATA["Tingkatan 2 (DLP)"] = {
+    "1.0 BIODIVERSITY": {
+        "1.1 Diversity of organism": ["1.1.1 Elaborate and communicate about biodiversity.", "1.1.2 Justify the needs to manage biodiversity effectively."],
+        "1.2 Classification of organisms": ["1.2.1 Differentiate organisms using a dichotomous key based on common characteristics.", "1.2.2 Characterise the major taxonomy group."]
+    },
+    "2.0 ECOSYSTEM": {
+        "2.1 Energy flow in ecosystem": ["2.1.1 Explain with examples of producer, consumer and decomposer.", "2.1.2 Interprete food chain and food web."],
+        "2.2 Nutrient cycle in the ecosystem": ["2.2.1 Elaborate and communicate about the role of living things in the oxygen and carbon cycles in the ecosystem.", "2.2.2 Justify the role of organisms in the water cycle of an ecosystem.", "2.2.3 Solve problems when there are interferences to the cycles caused by human activities."],
+        "2.3 Interdependence and interaction among organisms": ["2.3.1 Explain with examples the interdependence among living things and the environment to maintain a balanced ecosystem.", "2.3.2 Justify the importance of adaptations of living things to the environment.", "2.3.3 Communicate examples of interactions between organisms and apply these interactions in daily life.", "2.3.4 Separate the factors that affect the size of population in an ecosystem.", "2.3.5 Predict how the changes in ecosystem affect the existing resources and balance of the population."],
+        "2.4 The role of human in maintaining a balanced nature": ["2.4.1 Justify and communicate that man needs a stable and productive ecosystem to sustain life."]
+    },
+    "3.0 NUTRITION": {
+        "3.1 Classes of food": ["3.1.1 Elaborate and communicate about classes of food.", "3.1.2 Test the presence of starch, glucose, protein and fats in food."],
+        "3.2 Importance of a balanced diet": ["3.2.1 Elaborate and communicate about a balanced diet.", "3.2.2 Estimate calories of food intake in a meal and plan a balanced diet.", "3.2.3 Conduct a research and justify the importance of a balanced diet, exercise and a healthy lifestyle in order to maintain a healthy body."],
+        "3.3 Human's digestive system": ["3.3.1 Elaborate and communicate about digestion."],
+        "3.4 Process of absorption and transportation of digested food and defecation": ["3.4.1 Conduct an experiment to explain the absorption of the end products of digestion.", "3.4.2 Relate the function of digestive system, blood circulatory system and respiratory system.", "3.4.3 Elaborate and communicate about defecation."]
+    },
+    "4.0 HUMAN HEALTH": {
+        "4.1 Infectious and non-infectious diseases": ["4.1.1 Differentiate and communicate about infectious and non-infectious diseases.", "4.1.2 Explain how infectious diseases are spread.", "4.1.3 Separate the cause and spread of infectious diseases.", "4.1.4 Generate ideas on the mechanism to prevent the spread of infectious diseases."],
+        "4.2 Body defence system": ["4.2.1 Elaborate and communicate about the function of body defence system.", "4.2.2 Define antigens, antibodies and immunity.", "4.2.3 Justify the importance of immunisation.", "4.2.4 Differentiate passive immunity and active immunity.", "4.2.5 Justify good practices towards attaining strong immune system.", "4.2.6 Justify and communicate about the importance of immunisation and health level of individuals to the family, social, economy and nation."]
+    },
+    "5.0 WATER AND SOLUTION": {
+        "5.1 Physical characteristics of water": ["5.1.1 Elaborate and communicate about water.", "5.1.2 Carry out experiments and communicate about the water evaporation process in daily life."],
+        "5.2 Solution and rate of solubility": ["5.2.1 Explain with example the meaning of solution and solubility.", "5.2.2 Carry out experiment to determine the factors affecting the rate of solubility.", "5.2.3 Explain with examples the meaning of colloids in daily life.", "5.2.4 Elaborate and communicate the uses of water as a universal solvent in daily life and manufacturing industry.", "5.2.5 Demonstrate examples of organic solvent and their uses in daily life."],
+        "5.3 Water purification and water supply": ["5.3.1 Demonstrate the water purification method.", "5.3.2 Solve problems in getting water supply for daily life usage.", "5.3.3 Build a model and communicate about water supply system.", "5.3.4 Justify water sustainability as a key to healthy living."]
+    },
+    "6.0 ACID AND ALKALI": {
+        "6.1 Properties of acid and alkali": ["6.1.1 Defining operationally acid and alkali.", "6.1.2 Explain with examples of acidic and alkaline subtances.", "6.1.3 Demonstrate the technique to determine the strength of acid and alkali based on pH value.", "6.1.4 Identify the uses of acid and alkali in daily life."],
+        "6.2 Neutralisation": ["6.2.1 Explain the neutralization reaction.", "6.2.2 Explain with examples the use of neutralisation reaction in daily life."]
+    },
+    "7.0 ELECTRICITY AND MAGNETISM": {
+        "7.1 Electricity": ["7.1.1 Describe and communicate about energy.", "7.1.2 Explain and communicate about the existence of electrostatic charges.", "7.1.3 Explain with examples on electrostatic in daily life.", "7.1.4 Draw a conclusion that the flow of charges produce electric current.", "7.1.5 Characterise current, voltage and resistance and their units.", "7.1.6 Draw a conclusion on the relationship between current, voltage and resistance."],
+        "7.2 The flow of electric current in series circuit and parallel circuit": ["7.2.1 Elaborate and communicate about the flow of electric current in series circuit and parallel circuit."],
+        "7.3 Magnetism": ["7.3.1 Draw a conclusion about the characteristics of a magnet.", "7.3.2 Describe and communicate about electromagnet.", "7.3.3 Carry out an experiment and communicate about the uses of magnet and electromagnet in daily life."]
+    },
+    "8.0 FORCE AND MOTION": {
+        "8.1 Force": ["8.1.1 Elaborate and communicate about force.", "8.1.2 Explain that force has magnitude, direction and point of application.", "8.1.3 Measure force in S.I. unit.", "8.1.4 Explain with examples that every action force has an equal (same magnitude) reaction force but in the opposite direction."],
+        "8.2 Effects of force": ["8.2.1 Elaborate and communicate about the effects of force.", "8.2.2 Explain and communicate the relationship between the differences in densities and the effects of bouyancy in daily life.", "8.2.3 Classify and solve problems on levers based on the position of fulcrum, load and effort.", "8.2.4 Explain and communicate about the moment of force.", "8.2.5 Carry out an experiment and communicate about pressure and its application in daily life.", "8.2.6 Elaborate and communicate about gas pressure based on the kinetic theory of gas.", "8.2.7 Explain and communicate about the existance of atmospheric pressure and the effects of altitude on the magnitude of pressure.", "8.2.8 Explain the effects of depth on liquid pressure."]
+    },
+    "9.0 HEAT": {
+        "9.1 Temperature and heat": ["9.1.1 Make a comparison between heat and temperature."],
+        "9.2 Heat flow and thermal equilibrium": ["9.2.1 Explain how heat flows from a hot region to a cold region.", "9.2.2 Explain and communicate about heat flow in natural phenomena.", "9.2.3 Communicate about heat conductors and heat insulators and their uses in daily life."],
+        "9.3 Principle of expansion and contraction of matter": ["9.3.1 Explain how heat can cause the expansion and contraction in solid, liquid and gas.", "9.3.2 Communicate about the various uses of expansion and contraction of matter in daily life."],
+        "9.4 Relation between the types of surfaces of objects to heat absorption and emission": ["9.4.1 Demonstrate how dark, dull objects absorb heat better than white, shiny objects.", "9.4.2 Demonstrate how dark, dull objects radiate heat better than white, shiny objects.", "9.4.3 Conceptualise and design using the heat concept in daily life."]
+    },
+    "10.0 SOUND WAVES": {
+        "10.1 Characteristics of sound waves": ["10.1.1 communicate about the basic characteristics of sound waves."],
+        "10.2 Loudness and pitch of sound": ["10.2.1 Explain frequency and its unit, and amplitude of vibration.", "10.2.2 Relate frequency to pitch.", "10.2.3 Relate amplitude to loudness.", "10.2.4 Explain with examples loudness and pitch using musical instruments."],
+        "10.3 Phenomena and aplications of reflection of sound waves": ["10.3.1 Explain with example phenomena related to reflection of sound waves such as echo and Doppler effect.", "10.3.2 Explain with example the applications of reflection of sound waves.", "10.3.3 Elaborate and communicate about limitations of hearing for humans and animals.", "10.3.4 Explain with examples ways to overcome human limitations of hearing."]
+    },
+    "11.0 STARS AND GALAXIES IN THE UNIVERSE": {
+        "11.1 Stars and galaxies in the universe": ["11.1.1 Communicate about the characteristics of objects in space.", "11.1.2 Compare and contrast the characteristics of stars (including the Sun) and relate them to the obsevation of stars on the Earth."]
+    },
+    "12.0 SOLAR SYSTEM": {
+        "12.1 Solar System": ["12.1.1 Compare distances between the Sun and the planets in the Solar System using astronomical units (a.u.) and light years.", "12.1.2 Construct a table to compare and contrast the planets in the Solar System with the Earth.", "12.1.3 Explore the possible relationship based on the planets' characteristics and explain the relationship including anomalies that may arise.", "12.1.4 Reason and make analogies in hypothetical situations related to the Solar System.", "12.1.5 Justify the Earth as the most ideal planet for life based on data collected."]
+    },
+    "13.0 METEOROID, ASTEROID, COMET": {
+        "13.1 Other objects in the Solar System: Meteoroids, Asteroids and Comets": ["13.1.1 Communicate on other objects in the solar system, such as meteoroids, asteroids dan comets.", "13.1.2 Discuss the movements of meteoroids, asteroids and comets and their effects on the Earth based on data.", "13.1.3 Generate ideas on how to reduce or prevent the possibility of asteroids colliding with the Earth."]
+    }
+};
