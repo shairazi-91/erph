72c8a5b7293a7e9bcda2b41aa0805d36a8d40e41
shairazi-91 <muhdshairazi@gmail.com>
2026-02-20 06:26:26 +0800

    Create data_t2bm.js

---
+++ data_t2bm.js
@@ -0,0 +1,193 @@
+MASTER_DATA["Tingkatan 2"] = {
+    "1.0 BIODIVERSITI": {
+        "1.1 Kepelbagaian organisma": [
+            "1.1.1 Menghuraikan dan berkomunikasi mengenai biodiversiti.", 
+            "1.1.2 Menjustifikasikan keperluan pengurusan biodiversiti yang berkesan."
+        ],
+        "1.2 Pengelasan organisma": [
+            "1.2.1 Membezakan organisma dengan kekunci dikotomi berdasarkan ciri-ciri sepunya.", 
+            "1.2.2 Mencirikan kumpulan taksonomi utama."
+        ]
+    },
+    "2.0 EKOSISTEM": {
+        "2.1 Aliran tenaga dalam ekosistem": [
+            "2.1.1 Menerangkan dengan contoh pengeluar, pengguna dan pengurai.", 
+            "2.1.2 Menginterpretasi rantai makanan dan siratan makanan."
+        ],
+        "2.2 Kitar nutrien dalam ekosistem": [
+            "2.2.1 Menghuraikan dan berkomunikasi mengenai peranan benda hidup dalam kitar oksigen dan kitar karbon dalam ekosistem.", 
+            "2.2.2 Menjustifikasikan peranan organisma dalam kitar air sesuatu ekosistem.", 
+            "2.2.3 Menyelesaikan masalah apabila terdapat gangguan ke atas kitar disebabkan aktiviti manusia."
+        ],
+        "2.3 Saling bersandaran dan interaksi antara organisma": [
+            "2.3.1 Menerangkan dengan contoh saling bersandaran antara benda hidup dan persekitaran untuk mengekalkan ekosistem yang seimbang.", 
+            "2.3.2 Menjustifikasikan kepentingan penyesuaian benda hidup kepada persekitaran.", 
+            "2.3.3 Berkomunikasi mengenai contoh interaksi antara organisma dan mengaplikasi interaksi tersebut dalam kehidupan harian.", 
+            "2.3.4 Memisahkan faktor yang mempengaruhi saiz populasi dalam sesuatu ekosistem.", 
+            "2.3.5 Meramalkan bagaimana perubahan dalam ekosistem mempengaruhi sumber sedia ada dan keseimbangan populasi."
+        ],
+        "2.4 Peranan manusia dalam mengekalkan keseimbangan alam": [
+            "2.4.1 Menjustifikasikan dan berkomunikasi bahawa manusia memerlukan ekosistem yang stabil dan produktif untuk kelestarian hidup."
+        ]
+    },
+    "3.0 NUTRISI": {
+        "3.1 Kelas makanan": [
+            "3.1.1 Menghuraikan dan berkomunikasi mengenai kelas makanan.", 
+            "3.1.2 Menguji kehadiran kanji, glukosa, protein dan lemak dalam makanan."
+        ],
+        "3.2 Kepentingan gizi seimbang": [
+            "3.2.1 Menghuraikan dan berkomunikasi mengenai gizi seimbang.", 
+            "3.2.2 Menganggar kalori makanan yang diambil dalam satu hidangan dan merancang gizi seimbang.", 
+            "3.2.3 Menjalankan kajian dan menjustifikasikan kepentingan gizi seimbang, senaman dan gaya hidup sihat dalam mengekalkan kesihatan badan."
+        ],
+        "3.3 Sistem pencernaan manusia": [
+            "3.3.1 Menghuraikan dan berkomunikasi mengenai pencernaan."
+        ],
+        "3.4 Proses penyerapan dan pengangkutan hasil pencernaan serta penyahtinjaan": [
+            "3.4.1 Menjalankan eksperimen untuk menerangkan penyerapan hasil akhir pencernaan.", 
+            "3.4.2 Menghubungkaitkan fungsi sistem pencernaan, sistem peredaran darah dan sistem respirasi.", 
+            "3.4.3 Menghuraikan dan berkomunikasi mengenai penyahtinjaan."
+        ]
+    },
+    "4.0 KESIHATAN MANUSIA": {
+        "4.1 Penyakit berjangkit dan penyakit tidak berjangkit": [
+            "4.1.1 Membezakan dan berkomunikasi mengenai penyakit berjangkit dan penyakit tidak berjangkit.", 
+            "4.1.2 Menerangkan bagaimana penyakit berjangkit disebarkan.", 
+            "4.1.3 Memisahkan punca dan penularan penyakit berjangkit.", 
+            "4.1.4 Menjana idea mengenai mekanisme menghalang penularan penyakit berjangkit."
+        ],
+        "4.2 Sistem pertahanan badan": [
+            "4.2.1 Menghuraikan dan berkomunikasi mengenai fungsi sistem pertahanan badan.", 
+            "4.2.2 Mendefinisikan antigen, antibodi dan keimunan.", 
+            "4.2.3 Menjustifikasikan kepentingan imunisasi.", 
+            "4.2.4 Membezakan keimunan pasif dan keimunan aktif.", 
+            "4.2.5 Menjustifikasikan amalan yang baik ke arah mencapai sistem imun yang mantap.", 
+            "4.2.6 Menjustifikasikan dan berkomunikasi mengenai kepentingan imunisasi dan tahap kesihatan individu terhadap keluarga, sosial, ekonomi dan negara."
+        ]
+    },
+    "5.0 AIR DAN LARUTAN": {
+        "5.1 Sifat fizik air": [
+            "5.1.1 Menghuraikan dan berkomunikasi mengenai air.", 
+            "5.1.2 Menjalankan eksperimen dan berkomunikasi mengenai proses penyejatan air dalam kehidupan harian."
+        ],
+        "5.2 Larutan dan kadar keterlarutan": [
+            "5.2.1 Menerangkan dengan contoh maksud larutan dan keterlarutan.", 
+            "5.2.2 Menjalankan eksperimen untuk menentukan faktor yang mempengaruhi kadar keterlarutan.", 
+            "5.2.3 Menerangkan dengan contoh maksud koloid dalam kehidupan harian.", 
+            "5.2.4 Menghuraikan dan berkomunikasi mengenai kegunaan air sebagai pelarut semesta dalam kehidupan harian dan industri pembuatan.", 
+            "5.2.5 Menunjukkan contoh pelarut organik dan kegunaannya dalam kehidupan harian."
+        ],
+        "5.3 Pembersihan dan pembekalan air": [
+            "5.3.1 Menunjuk cara kaedah pembersihan air.", 
+            "5.3.2 Menyelesaikan masalah mendapatkan bekalan air untuk kegunaan harian.", 
+            "5.3.3 Membina model dan berkomunikasi mengenai sistem pembekalan air.", 
+            "5.3.4 Menjustifikasikan kelestarian air sebagai kunci kehidupan yang sihat."
+        ]
+    },
+    "6.0 ASID DAN ALKALI": {
+        "6.1 Sifat asid dan alkali": [
+            "6.1.1 Mendefinisikan secara operasi asid dan alkali.", 
+            "6.1.2 Menerangkan dengan contoh bahan berasid dan beralkali.", 
+            "6.1.3 Menunjuk cara teknik menentukan kekuatan asid dan alkali berdasarkan nilai pH.", 
+            "6.1.4 Mengenal pasti kegunaan asid dan alkali dalam kehidupan harian."
+        ],
+        "6.2 Peneutralan": [
+            "6.2.1 Menerangkan tindak balas peneutralan.", 
+            "6.2.2 Menerangkan dengan contoh penggunaan tindak balas peneutralan dalam kehidupan harian."
+        ]
+    },
+    "7.0 ELEKTRIK DAN KEMAGNETAN": {
+        "7.1 Elektrik": [
+            "7.1.1 Menghuraikan dan berkomunikasi mengenai tenaga.", 
+            "7.1.2 Menerangkan dan berkomunikasi mengenai kewujudan cas elektrostatistik.", 
+            "7.1.3 Menerangkan dengan contoh elektrostatistik dalam kehidupan harian.", 
+            "7.1.4 Merumuskan bahawa aliran cas menghasilkan arus elektrik.", 
+            "7.1.5 Mencirikan arus, voltan dan rintangan serta unitnya.", 
+            "7.1.6 Merumuskan perkaitan antara arus, voltan dan rintangan."
+        ],
+        "7.2 Pengaliran arus elektrik dalam litar bersiri dan litar selari": [
+            "7.2.1 Menghuraikan dan berkomunikasi mengenai pengaliran arus elektrik dalam litar bersiri dan litar selari."
+        ],
+        "7.3 Kemagnetan": [
+            "7.3.1 Merumuskan ciri-ciri magnet.", 
+            "7.3.2 Menghuraikan dan berkomunikasi mengenai elektromagnet.", 
+            "7.3.3 Menjalankan eksperimen dan berkomunikasi mengenai kegunaan magnet dan elektromagnet dalam kehidupan harian."
+        ]
+    },
+    "8.0 DAYA DAN GERAKAN": {
+        "8.1 Daya": [
+            "8.1.1 Menghuraikan dan berkomunikasi mengenai daya.", 
+            "8.1.2 Menerangkan bahawa daya mempunyai magnitud, arah dan titik aplikasi.", 
+            "8.1.3 Mengukur daya dalam unit S.I.", 
+            "8.1.4 Menerangkan dengan contoh bahawa setiap daya tindakan terdapat daya tindak balas yang sama magnitud tetapi bertentangan arah."
+        ],
+        "8.2 Kesan daya": [
+            "8.2.1 Menghuraikan dan berkomunikasi mengenai kesan daya.", 
+            "8.2.2 Menerangkan dan berkomunikasi mengenai perkaitan antara perbezaan ketumpatan dengan kesan keapungan dalam kehidupan harian.", 
+            "8.2.3 Mengelaskan dan menyelesaikan masalah tuas berdasarkan kedudukan fulkrum, beban dan daya.", 
+            "8.2.4 Menerangkan dan berkomunikasi mengenai momen daya.", 
+            "8.2.5 Menjalankan eksperimen dan berkomunikasi mengenai tekanan serta aplikasinya dalam kehidupan harian.", 
+            "8.2.6 Menghuraikan dan berkomunikasi mengenai tekanan gas berdasarkan teori kinetik gas.", 
+            "8.2.7 Menerangkan dan berkomunikasi mengenai kewujudan tekanan atmosfera dan kesan altitud kepada magnitud tekanan.", 
+            "8.2.8 Menerangkan kesan kedalaman terhadap tekanan cecair."
+        ]
+    },
+    "9.0 HABA": {
+        "9.1 Hubung kait suhu dengan haba": [
+            "9.1.1 Membuat perbandingan antara haba dengan suhu."
+        ],
+        "9.2 Pengaliran haba dan keseimbangan terma": [
+            "9.2.1 Menerangkan haba mengalir dari kawasan panas ke kawasan sejuk.", 
+            "9.2.2 Menerangkan dan berkomunikasi mengenai pengaliran haba dalam fenomena alam.", 
+            "9.2.3 Berkomunikasi mengenai konduktor haba dan penebat haba serta kegunaannya dalam kehidupan harian."
+        ],
+        "9.3 Prinsip pengembangan dan pengecutan jirim": [
+            "9.3.1 Menerangkan bagaimana haba menyebabkan pengembangan dan pengecutan dalam pepejal, cecair dan gas.", 
+            "9.3.2 Berkomunikasi mengenai pelbagai kegunaan pengembangan dan pengecutan jirim dalam kehidupan harian."
+        ],
+        "9.4 Hubung kait jenis permukaan objek dengan penyerapan dan pembebasan haba": [
+            "9.4.1 Menunjuk cara objek yang gelap dan kusam menyerap haba lebih baik daripada objek putih dan berkilat.", 
+            "9.4.2 Menunjuk cara objek yang gelap dan kusam membebaskan haba lebih baik daripada objek putih dan berkilat.", 
+            "9.4.3 Mengonsepkan dan mereka bentuk menggunakan konsep haba dalam kehidupan harian."
+        ]
+    },
+    "10.0 GELOMBANG BUNYI": {
+        "10.1 Ciri gelombang bunyi": [
+            "10.1.1 Berkomunikasi mengenai ciri asas gelombang bunyi."
+        ],
+        "10.2 Kenyaringan dan kelangsingan bunyi": [
+            "10.2.1 Menerangkan frekuensi dan unitnya serta amplitud getaran.", 
+            "10.2.2 Menghubungkaitkan frekuensi dengan kelangsingan.", 
+            "10.2.3 Menghubungkaitkan amplitud dengan kenyaringan.", 
+            "10.2.4 Menerangkan dengan contoh kenyaringan dan kelangsingan menggunakan alat muzik."
+        ],
+        "10.3 Fenomena dan aplikasi pantulan gelombang bunyi": [
+            "10.3.1 Menerangkan dengan contoh fenomena yang berkait dengan pantulan gelombang bunyi seperti gema dan kesan Doppler.", 
+            "10.3.2 Menerangkan dengan contoh aplikasi pantulan gelombang bunyi.", 
+            "10.3.3 Menghuraikan dan berkomunikasi mengenai had pendengaran bagi manusia dan haiwan.", 
+            "10.3.4 Menerangkan dengan contoh cara mengatasi had pendengaran manusia."
+        ]
+    },
+    "11.0 BINTANG DAN GALAKSI DALAM ALAM SEMESTA": {
+        "11.1 Bintang dan galaksi dalam alam semesta": [
+            "11.1.1 Berkomunikasi mengenai ciri-ciri objek angkasa.", 
+            "11.1.2 Membanding dan membezakan ciri-ciri bintang (termasuk Matahari) dan menghubungkaitkannya dengan pemerhatian bintang di Bumi."
+        ]
+    },
+    "12.0 SISTEM SURIA": {
+        "12.1 Sistem Suria": [
+            "12.1.1 Membandingkan jarak planet dalam Sistem Suria dari Matahari dengan menggunakan unit astronomi (a.u.) dan tahun cahaya.", 
+            "12.1.2 Membina jadual untuk membanding dan membezakan planet dalam Sistem Suria dengan Bumi.", 
+            "12.1.3 Meneroka hubungan yang mungkin berdasarkan ciri planet dan memberi penjelasan tentang hubungan tersebut termasuk anomali yang mungkin wujud.", 
+            "12.1.4 Menaakul dan membuat analogi dalam situasi hipotetikal berkaitan dengan Sistem Suria.", 
+            "12.1.5 Menjustifikasikan Bumi merupakan planet yang paling layak untuk kehidupan berdasarkan data yang dikumpul."
+        ]
+    },
+    "13.0 METEOROID, ASTEROID, KOMET": {
+        "13.1 Jasad lain dalam Sistem Suria iaitu Meteoroid, Asteroid dan Komet": [
+            "13.1.1 Berkomunikasi mengenai jasad lain dalam sistem suria iaitu meteoroid, asteroid dan komet.", 
+            "13.1.2 Membincangkan pergerakan meteoroid, asteroid dan komet serta kesannya terhadap Bumi berdasarkan data.", 
+            "13.1.3 Menjana idea bagaimana mengurangkan atau mengelakkan kemungkinan berlakunya perlanggaran asteroid dengan Bumi."
+        ]
+    }
+};
